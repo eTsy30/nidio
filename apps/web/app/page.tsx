cab6728016795699d9c59f7ad6910a444ff830ee
Evgen Tsygankov <tsy@mail.com>
2026-07-20 13:43:54 +0300

    repo: configure turborepo

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,12 +1,10 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
    
     <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
       
       <h1 className="text-4xl font-bold text-gray-900 dark:text-white">Welcome to Nidio</h1>
-      
+    
       </main>
   
   );
